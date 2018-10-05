7e0075f009f3079b13604694f15a926eb143706e
root <root@HYPERTOP.localdomain>
2018-10-05 18:56:50 +0300

    Added testSpeed and successor test; impoved generateValues

--- TestModule.py
+++ TestModule.py
@@ -2,7 +2,11 @@ import math
 import random
 import logging
 import sys
+import time
+from bintrees import AVLTree
+from bintrees import RBTree
 from VEBTree import VEBTree
+
 logging.basicConfig(stream = sys.stderr, level = logging.DEBUG)
 MAXINT = 2**15
 class VEBError(Exception):
@@ -72,7 +76,24 @@ class VEBRemovingError(VEBError):
 	def __init__(self, value: int = None):
 		self.value = value
 		self.message = "VEBTree could not remove value {}".format(self.value)
+class VEBSuccessorError(VEBError):
+	"""Exception raised for wrong removing of the value.
 	
+	Attributes:
+        value 	- value which raised exception
+	"""
+	def __init__(self, value: int = None):
+		self.value = value
+		self.message = "Value {} has wrong successor".format(self.value)
+class VEBPredecessorError(VEBError):
+	"""Exception raised for wrong removing of the value.
+	
+	Attributes:
+        value 	- value which raised exception
+	"""
+	def __init__(self, value: int = None):
+		self.value = value
+		self.message = "Value {} has wrong predecessor".format(self.value)
 class TestModule(object):
 	"""Module to test VEBTree
 	Attributes:
@@ -146,7 +167,7 @@ class TestModule(object):
 		"""
 		for subTree in tree.infoCluster:
 			try:
-				if subTree.universum != tree.sqrtUniversum():
+				if subTree.universum != tree.sqrtUni:
 					raise VEBUniversumError(tree.universum, subTree.universum, tree.sqrtUniversum())
 			except VEBUniversumError as error:
 				print (error.message)
@@ -164,7 +185,7 @@ class TestModule(object):
 		"""
 		if universum == None:
 			universum = random.randint(2, MAXINT)
-		self.tree = VEBTree(universum)
+		self.tree = VEBTree(universum, fill = True)
 		if self.tree.universum % 2 != 0:
 			return False
 		try:
@@ -181,6 +202,7 @@ class TestModule(object):
 			print ("Error during initializing summary cluster." + error.message)
 			return False
 		self.checkSubtree(self.tree.resume)
+		self.tree.resetTree()
 		return True
 	def _testInsertValue(self, value: int) -> bool:
 		"""Inserts 'value' into the vEB tree and checks whether it is placed correctly
@@ -215,6 +237,7 @@ class TestModule(object):
 					list of inserted values - if all values were successfully inserted
 					empty list 				- if even one value was not inserted into the vEB tree
 		"""
+		 
 		self.tree.resetTree(universum)
 		# insert whatever value to check whether it will be placed in min element of root VEBTree object
 		if values == None:
@@ -222,11 +245,11 @@ class TestModule(object):
 		if randomInsert == True:
 			random.shuffle(values)
 		for value in values:
-			# Generated values can definitely be inserted into the tree
+			# Generated values should definitely be inserted into the tree
 			if self._testInsertValue(value) == False:
 				return list()
 		return values
-	def _generateValues(self, values: list = None) -> list:
+	def _generateValues(self, values: list = None, fullRange = False) -> list:
 		"""Generates list with random length in range[2, tree.universum) with random values [0,self.treeUniversum)
 		Arguments:
 					values - list with int values or None
@@ -234,14 +257,19 @@ class TestModule(object):
 					list of random values in range [0, self.tree.universum				   - if 'values' was None
 					list of all values which are not included in the values from universum - if 'values' was not None
 		"""
-		length = random.randint(2, self.tree.universum - 1)
+		if fullRange == False:
+			length = random.randint(2, self.tree.universum - 1)
+		else:
+			length = self.tree.universum
 		if values == None:
 			values = []
 			randomValue = 0
-			for index in range(length):
-				randomValue = random.randint(0, self.tree.universum - 1)
-				if not (randomValue in values):
-					values.append(randomValue)
+			for value in range(0, self.tree.universum):
+				if len(values) == length:
+					break
+				randomValue = random.randint(0, 100)
+				if randomValue >= 50:
+					values.append(value)
 		else:
 			rightValues = values
 			values = []
@@ -267,27 +295,273 @@ class TestModule(object):
 		
 		for value in testValues:
 			try:
-				#pdb.set_trace()
 				if self.tree.removeValue(value) == True:
 					raise VEBRemovingError(value)
 			except VEBRemovingError as error:
 				print("VEBTree somehow removed nonexisted {} value".format(error.value))
-				#pdb.set_trace()
+				 
 				return False
+		 
 		for value in deleteValues:
 			try:
 				if self.tree.removeValue(value) == False:
 					raise VEBRemovingError(value)
 			except VEBRemovingError as error:
 				print(error.message)
+				 
 				return False
 		return True
+	def testSuccessor(self, universum: int = None) -> bool:
+		success = False
+		self.tree.resetTree()
+		answer = 0
+		# values are guaranteed in the order
+		values = self._generateValues()
+		predecessor = values[0]
+		self._testInsertValue(predecessor)
+		successor = self._random(values, 1)
+		self._testInsertValue(successor)
+		# when value is the least
+		answer = self.tree.getSuccessor(predecessor)
+		success = self._checkAnswer(answer, successor)
+		answer = self.tree.getPredecessor(successor)
+		success = success and self._checkAnswer(answer, predecessor)
+		# when there is only one element
+		absentValue = successor
+		self.tree.removeValue(successor)
+		answer = self.tree.getSuccessor(predecessor)
+		success = success and self._checkAnswer(answer)
 
+		answer = self.tree.getPredecessor(predecessor)
+		success = success and self._checkAnswer(answer)
+		prev = predecessor
+		predecessor = self.tree.universum - 1
+		# when value is the maximum
+		self._testInsertValue(predecessor)
+		answer = self.tree.getSuccessor(predecessor)
+		success = success and self._checkAnswer(answer)
+		answer = self.tree.getPredecessor(predecessor)
+		success = success and self._checkAnswer(answer, prev)
+		# when value is not in the tree
+		prev = predecessor - 1
+		answer = self.tree.getSuccessor(prev)
+		success = success and self._checkAnswer(answer, predecessor)
+		
+		return success
+	def _random(self, values: list = None, startIndex: int = 0):
+		rand = 0
+		if values == None:
+			rand = random.randint(startIndex, self.tree.universum)
+		else:
+			rand = values[random.randint(startIndex, len(values) - 1)]
+		return rand
+	def _checkAnswer(self, actualValue, answer: int = None, successor: bool = True):
+		try:
+			if actualValue != answer:
+				if successor == True:
+					raise VEBSuccessorError()
+				else:
+					raise VEBPredecessorError()
+		except VEBError as error:
+			print(error.message)
+			print ("Wrong value")
+			return False
+		return True
+	def complexTest(self, universum:int, outputFiles):
+		startTime = time.time()
+		self.tree = VEBTree(universum, True)
+		totalTime = float(time.time() - startTime)
+		outputFiles[0].write("VEBTree, full init\n")
+		outputFiles[0].write("{}\n".format(totalTime))
+
+		values = self._generateValues(fullRange = True)
+		random.shuffle(values)
+		startTime = time.time()
+		for value in values:
+			self.tree.insertValue(value)
+		totalTime = float(time.time() - startTime)
+				
+		outputFiles[0].write("{:.30f}\n".format(float(totalTime/universum)))
+
+		startTime = time.time()
+		for value in values:
+			self.tree.containsValue(value)
+		totalTime = float(time.time() - startTime)
+				
+		outputFiles[0].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		
+		startTime = time.time()
+		for value in values:
+			self.tree.getPredecessor(value)
+		totalTime = float(time.time() - startTime)
+
+		outputFiles[0].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		startTime = time.time()
+		for value in values:
+			self.tree.getSuccessor(value)
+		totalTime = float(time.time() - startTime)
+		
+		outputFiles[0].write("{:.30f} \n".format(float(totalTime/universum)))
+		
+		startTime = time.time()
+		for value in values:
+			self.tree.removeValue(value)
+		totalTime = float(time.time() - startTime)
+		
+		outputFiles[0].write("{:.30f} \n".format(float(totalTime/universum)))
+		
+		startTime = time.time()
+		self.tree = VEBTree(universum)
+		totalTime = float(time.time() - startTime)
+		outputFiles[1].write("VEBTree, lazy init\n")
+		outputFiles[1].write("{}\n".format(totalTime))
+
+		#values = self._generateValues()
+		#random.shuffle(values)
+		startTime = time.time()
+		for value in values:
+			self.tree.insertValue(value)
+		totalTime = float(time.time() - startTime)
+				
+		outputFiles[1].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		startTime = time.time()
+		for value in values:
+			self.tree.containsValue(value)
+		totalTime = float(time.time() - startTime)
+				
+		outputFiles[1].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		
+		startTime = time.time()
+		for value in values:
+			self.tree.getPredecessor(value)
+		totalTime = float(time.time() - startTime)
+
+		outputFiles[1].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		startTime = time.time()
+		for value in values:
+			self.tree.getSuccessor(value)
+		totalTime = float(time.time() - startTime)
+		
+		outputFiles[1].write("{:.30f} \n".format(float(totalTime/universum)))
+		
+		startTime = time.time()
+		for value in values:
+			self.tree.removeValue(value)
+		totalTime = float(time.time() - startTime)
+		outputFiles[1].write("{:.30f} \n".format(float(totalTime/universum)))
+		
+		startTime = time.time()
+		competitor = AVLTree()
+		outputFiles[2].write("AVLTree\n")
+		totalTime = float(time.time() - startTime)
+		outputFiles[2].write("{}\n".format(totalTime))
+		newValues = []
+		for value in range(0, len(values)):
+			newValues.append(random.randint(0, universum*2))
+		values = newValues
+		startTime = time.time()
+		for value in values:
+			competitor.insert(value, value)
+		totalTime = float(time.time() - startTime)
+		outputFiles[2].write("{:.30f} \n".format(float(totalTime/universum)))
+		startTime = time.time()
+		for value in values:
+			competitor.get_value(value)
+		totalTime = float(time.time() - startTime)
+		outputFiles[2].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		startTime = time.time()
+		for value in values:
+			try:
+				competitor.succ_key(value)
+			except:
+				pass
+		totalTime = float(time.time() - startTime)
+		outputFiles[2].write("{:.30f} \n".format(float(totalTime/universum)))
+		startTime = time.time()
+		for value in values:
+			try:
+				competitor.prev_key(value)
+			except:
+				pass
+		totalTime = float(time.time() - startTime)
+		outputFiles[2].write("{:.30f} \n".format(float(totalTime/universum)))
+		startTime = time.time()
+		for value in values:
+			try:
+				competitor.remove(value)
+			except:
+				pass
+		totalTime = float(time.time() - startTime)
+		outputFiles[2].write("{:.30f} \n".format(float(totalTime/universum)))
+		
+		
+		startTime = time.time()
+		competitor = RBTree()
+		outputFiles[3].write("RBTree\n")
+		totalTime = float(time.time() - startTime)
+		outputFiles[3].write("{}\n".format(totalTime))
+		startTime = time.time()
+		for value in values:
+			competitor.insert(value, value)
+		totalTime = float(time.time() - startTime)
+		outputFiles[3].write("{:.30f} \n".format(float(totalTime/universum)))
+		startTime = time.time()
+		for value in values:
+			competitor.get_value(value)
+		totalTime = float(time.time() - startTime)
+		outputFiles[3].write("{:.30f} \n".format(float(totalTime/universum)))
+
+		startTime = time.time()
+		for value in values:
+			try:
+				competitor.succ_key(value)
+			except:
+				pass
+		totalTime = float(time.time() - startTime)
+		outputFiles[3].write("{:.30f} \n".format(float(totalTime/universum)))
+		startTime = time.time()
+		for value in values:
+			try:
+				competitor.prev_key(value)
+			except:
+				pass
+		totalTime = float(time.time() - startTime)
+		outputFiles[3].write("{:.30f} \n".format(float(totalTime/universum)))
+		startTime = time.time()
+		for value in values:
+			try:
+				competitor.remove(value)
+			except:
+				pass
+		totalTime = float(time.time() - startTime)
+		outputFiles[3].write("{:.30f} \n".format(float(totalTime/universum)))
+	def testSpeed(self, unversum:int = None):
+		logFile = open("veb1_log.txt", 'w')
+		logFile1 = open("veb2_log.txt", 'w')
+		logFile2 = open("avl_log.txt", 'w')
+		logFile3 = open("rbtree_log.txt", 'w')
+		for power in range(2, 21):
+			line = "{} values\n".format(2**power)
+			logFile.write(line)
+			logFile1.write(line)
+			logFile2.write(line)
+			logFile3.write(line)
+			self.complexTest(2**power, [logFile, logFile1, logFile2, logFile3])
+		
+		
 def test():
 	testObj = TestModule()
 	logging.debug("'Initializing' test has passed {}".format(testObj.testInitFunction()))
 	logging.debug("'Incorrect Values Insertion' test has been passed successfully: {}".format(testObj.testIncorrectValues()))
-	values = testObj.testInsertion(randomInsert = True)
 	logging.debug("'Insertion' test has been passed successfully: {}".format(len(values) > 0))
+	 
 	logging.debug("'Removing' test has been passed successfully: {}".format(testObj.testRemoving(deleteValues = values)))
+	logging.debug("'Successor' test has been passed successfully: {}".format(testObj.testSuccessor()))
+	testObj.testSpeed()
 test()
